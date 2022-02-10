17a7c77483338aa240de392076a9579fc8ac05c3
Parth Arora <parth.arora1614@gmail.com>
2022-02-10 20:13:02 +0530

    Create LotteryNFTTicket.sol

---
+++ LotteryNFTTicket.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract NFTLOTTERY is ERC721, Pausable, AccessControl, ERC721Burnable {
+    bytes32 public constant CEO = keccak256("CEO");
+    bytes32 public constant CTO = keccak256("CTO");
+    bytes32 public constant CFO = keccak256("CFO");
+    
+    modifier validate() {
+        require(
+            hasRole(CEO, msg.sender) ||
+                hasRole(CFO, msg.sender) ||
+                hasRole(CTO, msg.sender),
+            "AccessControl: Address does not have valid Rights"
+        );
+        _;
+    }
+
+    constructor() ERC721("NFT LOTTERY", "LOTTO") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(CEO, msg.sender);
+    }
+
+    function pause() public validate{
+        _pause();
+    }
+
+    function unpause() public validate {
+        _unpause();
+    }
+
+    function safeMint(address to, uint256 tokenId) public validate {
+        if (_exists(tokenId)){
+            _burn(tokenId);
+        }
+        _safeMint(to, tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
