e64caba20f906ea8d8cd15203679ba5df7b95ad6
Pranav M S <pranavms13@yahoo.com>
2022-02-17 16:03:26 +0530

    Multiple Pools

--- contracts/Lotto.sol
+++ contracts/Lotto.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+// import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
 contract Lotto is Pausable, AccessControl{
 
@@ -14,15 +14,18 @@ contract Lotto is Pausable, AccessControl{
     address internal constant _safe = 0xd8806d66E24b702e0A56fb972b75D24CAd656821;
     // address internal _VRF = 0x3d2341ADb2D31f1c5530cDC622016af293177AE0;
     // address internal _LINK = 0xb0897686c545045aFc77CF20eC7A532E3120E0F1;
-    address public winner;
-    address[] public Players;
+    address[] public winners;
+    address[] public Pool1;
+    address[] public Pool5;
+    address[] public Pool10;
+    uint[3] public PoolAmounts;
+    bool[3] internal claimed;
 
     // uint internal _FEE = 0.0001 * 1e18;
     uint public winningNumber;
-    uint Ticket;
     uint startDay;
     uint lotteryPeriod;
-    uint claimperiod;
+    uint claimperiod = 0;
 
     bool internal _win;
 
@@ -74,77 +77,90 @@ contract Lotto is Pausable, AccessControl{
         return (uint(1e14) / price) * 1e12; // a dollar in matic
     }
 
-    function getFiveDollar() public view returns(uint256){
-        uint price = getPrice();
-        return ((uint(1e14) / price) * 1e12)*5; // Five dollar in matic
+    function getPot(uint _pool) public view returns(uint256){
+        if(_pool == 1) return PoolAmounts[0];
+        else if(_pool == 5) return PoolAmounts[1];
+        else if(_pool == 10) return PoolAmounts[2];
+        else return 0;
     }
 
-    function getTenDollar() public view returns(uint256){
-        uint price = getPrice();
-        return ((uint(1e14) / price) * 1e12)*10; // Ten dollar in matic
-    }
-
-    function getPot() public view returns(uint256){
-        return address(this).balance;
-    }
-
-    function getPotUSD() public view returns(uint256){
-        return (address(this).balance) * getPrice();
+    function getPotUSD(uint _pool) public view returns(uint256){
+        return (getPot(_pool)) * getPrice();
     }
 
     function getAnnounceDate() public view returns(uint256){
         return lotteryPeriod + 1;
     }
 
-    function getPlayerCount() public view returns(uint256){
-        return Players.length;
+    function getPlayerCount(uint _pool) public view returns(uint256){
+        if(_pool == 1){
+            return Pool1.length;
+        }else if(_pool == 5){
+            return Pool5.length;
+        }else if(_pool == 10){
+            return Pool10.length;
+        }else return 0;
+    }
+
+    function getTotalPlayerCount() public view returns(uint256){
+        return Pool1.length + Pool5.length + Pool10.length;
     }
 
     function startLottery(uint _start, uint _duration) public validate {
+        resetLottery();
         startDay = _start;
         lotteryPeriod = startDay + _duration * 1 days;
         claimperiod = lotteryPeriod + 2 days;
     }
 
-    function updateLottery() public validate {
-        startLottery(claimperiod, 5);
+    function resetLottery() public validate {
+        // startLottery(claimperiod, 5);
+        require(block.timestamp > claimperiod, "Cannot reset Lottery before prevoius Claim period end");
+        Pool1 = [address(0)];
+        Pool5 = [address(0)];
+        Pool10 = [address(0)];
+        PoolAmounts = [0,0,0];
+        winners = [address(0)];
     }
 
-    // Seperate the NFT Issuing part with the loop into a function
-
-    function BuyTicket() public payable{
+    function BuyOneTicket() public payable{
         require(block.timestamp < lotteryPeriod, "Lottery Period Ended: No More buying allowed");
-        //---------
-        uint price = getPrice();
-        uint onedollar = (uint(1e14) / price) * 1e12; // a dollar in matic
+        uint onedollar = getDollar();
+        require(msg.value >= onedollar, "Buy Lottery Ticket: Price should be greater than 1 USD");
+        Pool1.push(msg.sender); // Pool1 is the 1 USD pool
+        LottoTickets.safeMint(msg.sender, (Pool1.length * 10) + 0);
+        PoolAmounts[0] = PoolAmounts[0] + msg.value - gasleft();
+    }
 
-        require(msg.value >= onedollar, "BTS: Price should be greater than a dollar");
+    function BuyFiveTicket() public payable{
+        require(block.timestamp < lotteryPeriod, "Lottery Period Ended: No More buying allowed");
+        uint onedollar = getDollar();
+        require(msg.value >= (onedollar * 5), "Buy Lottery Ticket: Price should be greater than 5 USD");
+        Pool5.push(msg.sender); // Pool5 is the 5 USD pool
+        LottoTickets.safeMint(msg.sender, (Pool5.length * 10) + 1);
+        PoolAmounts[1] = PoolAmounts[1] + msg.value - gasleft();
+    }
 
-        if(msg.value >= (onedollar * 10)){
-            uint j=1;
-            while(j <= 10){
-                Players[++Ticket] = msg.sender;
-                LottoTickets.safeMint(msg.sender, Ticket);
-                j += 1;
-            }
-        }else if(msg.value >= (onedollar * 5)){
-            uint j=1;
-            while(j <= 10){
-                Players[++Ticket] = msg.sender;
-                LottoTickets.safeMint(msg.sender, Ticket);
-                j += 1;
-            }
-        }else{
-            Players[++Ticket] = msg.sender;
-            LottoTickets.safeMint(msg.sender, Ticket);
-        }
+    function BuyTenTicket() public payable{
+        require(block.timestamp < lotteryPeriod, "Lottery Period Ended: No More buying allowed");
+        uint onedollar = getDollar();
+        require(msg.value >= (onedollar * 10), "Buy Lottery Ticket: Price should be greater than 10 USD");
+        Pool10.push(msg.sender); // Pool10 is the 10 USD pool
+        LottoTickets.safeMint(msg.sender, (Pool10.length * 10) + 2);
+        PoolAmounts[2] = PoolAmounts[2] + msg.value - gasleft();
     }
 
     function AnnounceLotteryWinner() public validate{
         require(block.timestamp > lotteryPeriod, "Lottery Period Not Ended: No winners yet");
         require(_win == false, "Cannot Announce winner yet");
-        winningNumber = uint(blockhash(block.number - 1)) % Players.length;
-        winner = Players[winningNumber];
+        // winningNumber = uint(blockhash(block.number - 1)) % Players.length;
+
+        uint winningOne = uint(blockhash(block.number - 1)) % Pool1.length;
+        uint winningFive = uint(blockhash(block.number - 1)) % Pool5.length;
+        uint winningTen = uint(blockhash(block.number - 1)) % Pool10.length;
+        winners.push(Pool1[winningOne]);
+        winners.push(Pool5[winningFive]);
+        winners.push(Pool10[winningTen]);
         _win = true;
     }
 
@@ -155,17 +171,25 @@ contract Lotto is Pausable, AccessControl{
 
         require(block.timestamp > lotteryPeriod, "Lottery Period: Still buying tickets");
         require(block.timestamp < claimperiod, "Claim Period: Invalid Claim Period");
-        require(msg.sender == winner, "Error: You are not the winner");
+        // require(msg.sender == winner, "Error: You are not the winner");
+        require((msg.sender == winners[0]) || (msg.sender == winners[1]) || (msg.sender == winners[2]), "Error: You are not the winner");
         
-        uint cut = address(this).balance * 5 / 100;
-        uint pot = address(this).balance - cut;
-        payable(_safe).transfer(cut);
-        payable(winner).transfer(pot);
-        updateLottery();
+        uint winnerpot;
+        if(winners[0] == msg.sender){winnerpot = 0;} // 1 USD Pot
+        else if(winners[1] == msg.sender){winnerpot = 1;}  // 5 USD Pot
+        else if(winners[2] == msg.sender){winnerpot = 2;}  // 10 USD Pot
 
-        winningNumber = 0;
-        winner = address(0);
-        _win = false;
+        uint poolpot = PoolAmounts[winnerpot];
+        require(claimed[winnerpot] == false, "Claim: Prize already Claimed");
+
+        uint cut = poolpot * 5 / 100;
+        uint pot = poolpot - cut;
+
+        payable(_safe).transfer(cut);
+        payable(winners[winnerpot]).transfer(pot);
+
+        claimed[winnerpot] = true;
+        // _win = false;
     } 
 }
 
